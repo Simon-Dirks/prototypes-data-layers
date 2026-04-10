f831cf04bfde5195291ed8d285ccff161f0e3744
sdevalk <sjors@sjorsdevalk.nl>
2026-04-10 22:46:49 +0200

    improved scripts

--- apps/valeros-cli/README.md
+++ apps/valeros-cli/README.md
@@ -6,6 +6,10 @@ CLI for the data layer of Valeros. For use in development only.
 
     docker build -t valeros-cli .
 
-### Run
+## Run
 
     docker run -i --rm -v "$PWD":/app -t valeros-cli
+
+## Prepare datasets for ingestion into the search index
+
+    scripts/prepare.sh --url "https://collections.uu.nl/datadump_28-03-2026.jsonld.gz" --query-file "queries/collections-uu.rq" --output-file "data/collections-uu.jsonld"
--- apps/valeros-cli/scripts/prepare.sh
+++ apps/valeros-cli/scripts/prepare.sh
@@ -2,83 +2,21 @@
 
 set -euo pipefail
 
-prepareDistribution() {
-  local url="$1"
-  local outputFile="$2"
+SCRIPTS_DIR=$(cd $(dirname $0) && pwd -P)
 
-  echo "Downloading, validating and converting '$url'"
+source $SCRIPTS_DIR/functions.sh
 
-  baseFileName=$(basename "$url")
+URL=
+QUERY_FILE=
+OUTPUT_FILE=
 
-  wget -q $url -O "/tmp/$baseFileName"
+# Parse command line arguments
+while [[ "$#" > 1 ]]; do case $1 in
+  --url) URL="$2";;
+  --query-file) QUERY_FILE="$2";;
+  --output-file) OUTPUT_FILE="$2";;
+  *) break;;
+  esac; shift; shift
+done
 
-  # Test for gzip
-  if gzip -t "/tmp/$baseFileName" 2>/dev/null; then
-    gunzip -f "/tmp/$baseFileName"  # Remove the .gz suffix automatically
-    baseFileName="${baseFileName%.*}" # Remove the *last* dot‑extension, i.e. `.gz`
-  fi
-
-  extension="${outputFile##*.}" # E.g. `nq`
-
-  # Validate and convert the distribution
-  riot --output $extension "/tmp/$baseFileName" > "$outputFile"
-}
-
-mapRdfToRdf() {
-  local inputFile="$1"
-  local queryFile="$2"
-  local outputFile="$3"
-
-  echo "Mapping data in '$inputFile' to '$outputFile' according to '$queryFile'"
-
-  # Remove existing file, if any
-  rm -f $outputFile
-
-  extension="${outputFile##*.}" # E.g. `ttl`
-
-  sparql --data "$inputFile" --query "$queryFile" --results $extension > "$outputFile"
-}
-
-toJsonLd() {
-  local inputFile="$1"
-  local outputFile="$2"
-
-  echo "Transforming data in '$inputFile' to JSON-LD in '$outputFile'..."
-
-  # Remove existing file, if any
-  rm -f $outputFile
-
-  riot --output jsonld "$inputFile" > "$outputFile"
-}
-
-prepare() {
-  local url="$1"
-  local queryFile="$2"
-  local jsonldFile="$3"
-  local startTime=$SECONDS
-
-  echo "Preparing data from '$url'..."
-
-  outputDirName=$(dirname "$jsonldFile")
-  outputFileName=$(basename "$jsonldFile")
-
-  nquadsFile="$outputDirName/$outputFileName.nq"
-  turtleFile="$outputDirName/$outputFileName.ttl"
-
-  # Optimization for testing: do not prepare again if the file already exists
-  if [[ ! -e "$nquadsFile" ]]; then
-    prepareDistribution "$url" "$nquadsFile"
-  fi
-
-  mapRdfToRdf "$nquadsFile" "$queryFile" "$turtleFile"
-  toJsonLd "$turtleFile" "$jsonldFile"
-
-  rm -f "$turtleFile"
-
-  local endTime=$SECONDS
-  local duration=$((endTime-startTime))
-
-  echo "Done preparing in $duration seconds"
-}
-
-prepare "https://collections.uu.nl/datadump_28-03-2026.jsonld.gz" "./queries/collections-uu.rq" "./data/collections-uu.jsonld"
+prepare "$URL" "$QUERY_FILE" "$OUTPUT_FILE"
